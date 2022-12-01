e50cab63462a0d32b366ff0dab627e1fcfe371fb
kazadi11 <kapitcho92@gmail.com>
2022-12-01 14:12:39 +0100

    CP

---
+++ itest.php
@@ -0,0 +1,21 @@
+<?php
+echo "cette fois mon php est <strong>interpreter</strong>";
+
+$chaine = "cest le cours de logique";
+/*la FONCTION*/
+echo"la longeur" <div class="strLEN"></di
+CHAINE;v>
+?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    ?exif_imagetype?><div class="DIV>!"></div>
+</body>%variant_pow12>?$_SESSION
+</html>
